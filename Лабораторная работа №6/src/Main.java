25d5ed0a5f66d38f184db9223771c60b3ac1df6c
feddot2517 <36929352+feddot2517@users.noreply.github.com>
2019-05-17 09:45:49 +0600

    Add files via upload

---
+++ Лабораторная работа №6/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Array s = new Array();
+        s.createArray();
+        A abcd = new A();
+        abcd.mapping();
+        abcd.out(10);
+        abcd.outall();
+    }
+}
